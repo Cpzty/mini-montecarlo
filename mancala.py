1906a058a17278dcbcdfa3329229b92a1e8dcd30
Cristian Pérez <Cristianpzty@gmail.com>
2019-10-04 20:48:49 -0600

    terminadas jugadas

--- mancala.py
+++ mancala.py
@@ -5,6 +5,7 @@ class Mancala:
     def __init__(self, state, player):
         self.state = state
         self.player = player
+        self.patch = 0
     #valid moves
     def valid_moves(self,state,player):
         moves = []
@@ -22,17 +23,68 @@ class Mancala:
     def make_move(self, move):
         #revisar primero si cae en score
         beads = self.state[move]
+        final_position = move + beads
         if self.player == 1:
-            if (move + beads) % 6 == 0:
+            #caso 1
+            #ultimo bead cae en score
+            #jugador vuelve a mover
+            if (final_position) % 6 == 0:
                 #remove beads from current place
                 self.state[move] = 0
-                #add beads to score
-                self.state[6] = self.state[6] + beads
+                for i in range(1, beads + 1):
+                    scyther = (move + i) % 12
+                    self.state[scyther] = self.state[scyther] + 1
+                return 1
+
+            #ultimo caso o caso sencillo
+            #se reparte 1 bead a cada posicion
             else:
+                self.state[move] = 0
                 for i in range(1, beads+1):
                     scyther = (move + i) % 12
                     self.state[scyther] = self.state[scyther] + 1
+                    # subcaso el ultimo bead cae una casilla vacia
+                    # se añaden el ultimo bead del jugador y los beads del enemigo a score del jugador
+                if ((final_position) % 12 < 6 and self.state[final_position] == 1):
+                    mirror = 12 - final_position
+                    self.state[6] = self.state[6] + self.state[mirror] + 1
+                    self.state[mirror] = 0
+                    self.state[final_position] = 0
+                return 2
+        else:
+            if (final_position) % 13 == 0:
+                #remove beads from current place
+                self.state[move] = 0
+                self.patch = 0
+                for i in range(1, beads + 1):
+                    scyther = (move + i + self.patch) % 13
+                    if scyther != 6:
+                        self.state[scyther] = self.state[scyther] + 1
+                    else:
+                        self.state[scyther + 1] = self.state[scyther + 1] + 1
+                        self.patch = self.patch + 1
+                return 2
 
+            #ultimo caso o caso sencillo
+            #se reparte 1 bead a cada posicion
+            else:
+                self.state[move] = 0
+                self.patch = 0
+                for i in range(1, beads + 1):
+                    scyther = (move + i + self.patch) % 13
+                    if scyther != 6:
+                        self.state[scyther] = self.state[scyther] + 1
+                    else:
+                        self.state[scyther + 1] = self.state[scyther + 1] + 1
+                        self.patch = self.patch + 1
+                    # subcaso el ultimo bead cae una casilla vacia
+                    # se añaden el ultimo bead del jugador y los beads del enemigo a score del jugador
+                if ((final_position) % 13 > 6 and self.state[final_position] == 1):
+                    mirror = 12 - final_position
+                    self.state[13] = self.state[13] + self.state[mirror] + 1
+                    self.state[mirror] = 0
+                    self.state[final_position] = 0
+                return 1
 
 game = Mancala([4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0], 1)
 human = choice(select_player)
@@ -50,8 +102,7 @@ while True:
         if(human == 1):
             print("player turn")
             decision = int(input("what is your move?: "))
-            game.make_move(decision)
-            game.player = 2
+            game.player = game.make_move(decision)
         else:
             print("bot is making a choice")
             game.player = 2
@@ -59,8 +110,8 @@ while True:
         if(human == 2):
             print("player turn")
             decision = int(input("what is your move?: "))
-            game.make_move(decision)
-            game.player = 1
+            game.player = game.make_move(decision)
+
         else:
             print("bot is making a move...")
             game.player = 1

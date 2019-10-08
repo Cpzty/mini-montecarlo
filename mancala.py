eea38a2e41828832e3c6004b6c0f67f19f031268
Cristian Pérez <Cristianpzty@gmail.com>
2019-10-08 14:00:02 -0600

    falta testear

--- mancala.py
+++ mancala.py
@@ -1,4 +1,5 @@
 from random import random, randint, choice
+from copy import deepcopy
 select_player = [1, 2]
 
 class Mancala:
@@ -7,16 +8,19 @@ class Mancala:
         self.player = player
         self.patch = 0
         self.finish = False
+        self.fake_state = []
+
+
     #valid moves
-    def valid_moves(self):
+    def valid_moves(self, state):
         moves = []
         if self.player == 1:
             for i in range(6):
-                if(self.state[i] !=0):
+                if(state[i] !=0):
                     moves.append(i)
         else:
             for i in range(7, 13):
-                if(self.state[i] !=0):
+                if(state[i] !=0):
                     moves.append(i)
         #print(moves)
         return moves
@@ -87,10 +91,55 @@ class Mancala:
                     self.state[final_position % 14] = 0
                 return 1
 
+    def make_move_fake(self, move, state):
+        #revisar primero si cae en score
+        beads = self.fake_state[move]
+        final_position = move + beads
+        if self.player == 1:
+            #caso 1
+            #ultimo bead cae en score
+            #jugador vuelve a mover
+            if (final_position) % 6 == 0:
+                #remove beads from current place
+                self.fake_state[move] = 0
+                for i in range(1, beads + 1):
+                    scyther = (move + i) % 13
+                    self.fake_state[scyther] = self.fake_state[scyther] + 1
+                return 1
+
+            #ultimo caso o caso sencillo
+            #se reparte 1 bead a cada posicion
+            else:
+                self.fake_state[move] = 0
+                for i in range(1, beads+1):
+                    scyther = (move + i) % 13
+                    self.fake_state[scyther] = self.fake_state[scyther] + 1
+                    # subcaso el ultimo bead cae una casilla vacia
+                    # se añaden el ultimo bead del jugador y los beads del enemigo a score del jugador
+                if ((final_position) % 14 < 6 and self.fake_state[final_position] == 1):
+                    mirror = 12 - (final_position % 13)
+                    self.fake_state[6] = self.fake_state[6] + self.fake_state[mirror] + 1
+                    self.fake_state[mirror] = 0
+                    self.fake_state[final_position % 13] = 0
+                return 2
+        else:
+            if (final_position) % 13 == 0:
+                #remove beads from current place
+                state[move] = 0
+                self.patch = 0
+                for i in range(1, beads + 1):
+                    scyther = (move + i + self.patch) % 14
+                    if scyther != 6:
+                        state[scyther] = state[scyther] + 1
+                    else:
+                        state[scyther + 1] = state[scyther + 1] + 1
+                        self.patch = self.patch + 1
+                return 2
+
+
     def terminal_state(self):
         if(self.state[0] + self.state[1] + self.state[2] + self.state[3] + self.state[4] + self.state[5] == 0
         or self.state[7] + self.state[8] + self.state[9] + self.state[10] + self.state[11] + self.state[12] == 0):
-
             #quien termina el juego no es revisado bajo este pensamiento y por lo tanto se agregan los beads de cada jugador a su score aunque
             self.state[6] = self.state[6] + self.state[0] + self.state[1] + self.state[2] + self.state[3] + self.state[4] + self.state[5]
             self.state[13] = self.state[13] + self.state[7] + self.state[8] + self.state[9] + self.state[10] + self.state[11] + self.state[12]
@@ -99,7 +148,77 @@ class Mancala:
             self.state[7] = self.state[8] = self.state[9] = self.state[10] = self.state[11] = self.state[12] = 0
             self.finish = True
 
-def print_game(state, player):  
+    def terminal_state_fake(self, state):
+        if(state[0] + state[1] + state[2] + state[3] + state[4] + state[5] == 0
+        or state[7] + state[8] + state[9] + state[10] + state[11] + state[12] == 0):
+            #quien termina el juego no es revisado bajo este pensamiento y por lo tanto se agregan los beads de cada jugador a su score aunque
+            state[6] = state[6] + state[0] + state[1] + state[2] + state[3] + state[4] + state[5]
+            state[13] = state[13] + state[7] + state[8] + state[9] + state[10] + state[11] + state[12]
+            #set to 0
+            state[0] = state[1] = state[2] = state[3] = state[4] = state[5] = 0
+            state[7] = state[8] = state[9] = state[10] = state[11] = state[12] = 0
+            return True
+        return False
+
+
+    # montecarlo
+    def montecarlo_search_tree(self):
+        temp_state = deepcopy(self.state)
+        count7 = count8 = count9 = count10 = count11 = count12 = 0
+        juego_termino = False
+        jugadas_validas = self.valid_moves(self.state)
+        for i in range(len(jugadas_validas)):
+            #refresh state after each option is completed
+            self.fake_state = deepcopy(temp_state)
+            for j in range(1000):
+                while juego_termino == False:
+
+                        # if(human == 1):
+                        #decision = int(input("what is your move? (0-5): "))
+                    bot1_moves = self.valid_moves(self.fake_state)
+                    self.make_move_fake(choice(bot1_moves), self.fake_state)
+                    if self.player == 1:
+                        self.player = 2
+                    else:
+                        self.player = 1
+                    #print(self.player)
+                    print(self.valid_moves(self.fake_state))
+                    juego_termino = self.terminal_state_fake(self.fake_state)
+                if(jugadas_validas[i] == 7):
+                    if(self.fake_state[13] > self.fake_state[6]):
+                        count7 = count7 + 1
+                    elif(self.fake_state[6] > self.fake_state[13]):
+                        count7 = count7 - 1
+                elif(jugadas_validas[i] == 8):
+                    if (self.fake_state[13] > self.fake_state[6]):
+                        count8 = count8 + 1
+                    elif (self.fake_state[6] > self.fake_state[13]):
+                        count8 = count8 - 1
+                elif (jugadas_validas[i] == 9):
+                    if (self.fake_state[13] > self.fake_state[6]):
+                        count9 = count9 + 1
+                    elif (self.fake_state[6] > self.fake_state[13]):
+                        count9 = count9 - 1
+                elif (jugadas_validas[i] == 10):
+                    if (self.fake_state[13] > self.fake_state[6]):
+                        count10 = count10 + 1
+                    elif (self.fake_state[6] > self.fake_state[13]):
+                        count10 = count10 - 1
+                elif (jugadas_validas[i] == 11):
+                    if (self.fake_state[13] > self.fake_state[6]):
+                        count11 = count11 + 1
+                    elif (self.fake_state[6] > self.fake_state[13]):
+                        count11 = count11 - 1
+                elif (jugadas_validas[i] == 12):
+                    if (self.fake_state[13] > self.fake_state[6]):
+                        count12 = count12 + 1
+                    elif (self.fake_state[6] > self.fake_state[13]):
+                        count12 = count12 - 1
+
+        return [count7, count8, count9, count10, count11, count12]
+
+
+def print_game(state, player):
     if(player == 2):
         print("\n")
         print("|", state[6], "| ", state[0:6])
@@ -130,7 +249,8 @@ game = Mancala([4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0], 1)
 
 #all tests passed
 
-human = choice(select_player)
+#human = choice(select_player)
+human = 1
 if(human == 1):
     bot = 2
 else:
@@ -143,32 +263,34 @@ while game.finish == False:
     print_game(game.state, human)
     #print(game.state)
     if game.player == 1:
-        if(human == 1):
-            print("player turn")
-            decision = int(input("what is your move? (0-5): "))
-            referee = game.valid_moves()
-            if decision in referee:
-                game.player = game.make_move(decision)
-            else:
-                print("invalid move")
-                continue
+        #if(human == 1):
+        print("player turn")
+        decision = int(input("what is your move? (0-5): "))
+        referee = game.valid_moves(game.state)
+        if decision in referee:
+            game.player = game.make_move(decision)
         else:
-            print("bot is making a choice")
-            game.player = 2
+            print("invalid move")
+            continue
     else:
-        if(human == 2):
-            print("player turn")
-            decision = int(input("what is your move? (7-12): "))
-            referee = game.valid_moves()
-            if decision in referee:
-                game.player = game.make_move(decision)
-            else:
-                print("invalid move")
-                continue
-
-        else:
-            print("bot is making a move...")
-            game.player = 1
+        print("bot is making a choice")
+        monte = game.montecarlo_search_tree()
+        bot_choice = monte.index(max(monte)) + 7
+        game.player = game.make_move(bot_choice)
+    # else:
+    #     if(human == 2):
+    #         print("player turn")
+    #         decision = int(input("what is your move? (7-12): "))
+    #         referee = game.valid_moves()
+    #         if decision in referee:
+    #             game.player = game.make_move(decision)
+    #         else:
+    #             print("invalid move")
+    #             continue
+    #
+    #     else:
+    #         print("bot is making a move...")
+    #         game.player = 1
     game.terminal_state()
 print(game.state)
 if(game.state[6] > game.state[13]):
